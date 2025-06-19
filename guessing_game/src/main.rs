c4b7ea84f1303ac91a010b81b52a8b76854df76d
Marcos Cervetto <cervettomarcos@gmail.com>
2025-06-18 22:31:55 -0300

    Add initial Rust projects and update dependencies

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,11 +1,11 @@
-use rand::Rng;
+use rand::random_range;
 use std::cmp::Ordering;
 use std::io;
 
 fn main() {
     println!("Guess the number!");
 
-    let secret_number = rand::thread_rng().gen_range(1..=100);
+    let secret_number = random_range(1..=100);
 
     loop {
         println!("Please input your guess.");
