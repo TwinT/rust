ecfeb752ca842f2bd50db7428af503779b77088a
Marcos Cervetto <cervettomarcos@gmail.com>
2026-06-02 21:48:29 -0300

    hashmap

--- rustlings/exercises/11_hashmaps/hashmaps3.rs
+++ rustlings/exercises/11_hashmaps/hashmaps3.rs
@@ -31,6 +31,12 @@ fn build_scores_table(results: &str) -> HashMap<&str, TeamScores> {
         // Keep in mind that goals scored by team 1 will be the number of goals
         // conceded by team 2. Similarly, goals scored by team 2 will be the
         // number of goals conceded by team 1.
+        let t1_score = scores.entry(team_1_name).or_default();
+        t1_score.goals_scored += team_1_score;
+        t1_score.goals_conceded += team_2_score;
+        let t2_score = scores.entry(team_2_name).or_default();
+        t2_score.goals_scored += team_2_score;
+        t2_score.goals_conceded += team_1_score;
     }
 
     scores
@@ -54,9 +60,11 @@ England,Spain,1,0";
     fn build_scores() {
         let scores = build_scores_table(RESULTS);
 
-        assert!(["England", "France", "Germany", "Italy", "Poland", "Spain"]
-            .into_iter()
-            .all(|team_name| scores.contains_key(team_name)));
+        assert!(
+            ["England", "France", "Germany", "Italy", "Poland", "Spain"]
+                .into_iter()
+                .all(|team_name| scores.contains_key(team_name))
+        );
     }
 
     #[test]
