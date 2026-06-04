3d9d29eead4e8c72dd30267018f9c8bb17fe8d31
Marcos Cervetto <cervettomarcos@gmail.com>
2026-06-03 23:06:12 -0300

    errors

--- rustlings/exercises/13_error_handling/errors4.rs
+++ rustlings/exercises/13_error_handling/errors4.rs
@@ -1,3 +1,5 @@
+use std::cmp::Ordering;
+
 #[derive(PartialEq, Debug)]
 enum CreationError {
     Negative,
@@ -11,7 +13,11 @@ impl PositiveNonzeroInteger {
     fn new(value: i64) -> Result<Self, CreationError> {
         // TODO: This function shouldn't always return an `Ok`.
         // Read the tests below to clarify what should be returned.
-        Ok(Self(value as u64))
+        match value.cmp(&0) {
+            Ordering::Less => Err(CreationError::Negative),
+            Ordering::Equal => Err(CreationError::Zero),
+            Ordering::Greater => Ok(PositiveNonzeroInteger(value as u64)),
+        }
     }
 }
 
